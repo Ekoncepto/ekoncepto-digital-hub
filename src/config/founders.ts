4c63b3c723c99c7b5e07eaa8a2b5cfe4377b98ad
mfa326 <63646232+mfa326@users.noreply.github.com>
2025-11-13 06:43:20 -0300

    Fixes missing comma

--- src/config/founders.ts
+++ src/config/founders.ts
@@ -5,7 +5,7 @@ export const founders = [
     bio: 'Victor Amorim é o fundador da E-Koncepto e especialista em aceleração de vendas online para indústrias e comércios.\n \
 Com mais de 10 anos de experiência, atua como Vice-Presidente da Fiolux — uma das maiores metalúrgicas do país — e já gerou crescimento de 7x no faturamento e na lucratividade da empresa.\n \
 Ao longo da carreira, liderou a estruturação de operações digitais com faturamento superior a R$ 2 milhões mensais no Mercado Livre e expansão nacional de canais de venda.\n\
-Hoje, aplica essa expertise comprovada para acelerar os resultados dos clientes da E-Koncepto com estratégias práticas e rentáveis de e-commerce e marketplace.'
+Hoje, aplica essa expertise comprovada para acelerar os resultados dos clientes da E-Koncepto com estratégias práticas e rentáveis de e-commerce e marketplace.',
     image: '/images/victor-amorim.jpeg',
     linkedin: 'https://www.linkedin.com/in/victor-amorim-b1877a15b/',
     instagram: 'https://www.instagram.com/victoracelera/',
